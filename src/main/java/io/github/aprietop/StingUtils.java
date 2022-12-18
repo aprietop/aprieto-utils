93051f27a4bb8309c7a33a9de0bc6898cdaf18f3
Armando Prieto <aprieto@cryoport.com>
2022-12-17 16:59:36 -0800

    init

---
+++ src/main/java/io/github/aprietop/StingUtils.java
@@ -0,0 +1,32 @@
+package io.github.aprietop;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StingUtils {
+
+/**
+ * <p>Strips whitespace from the start and end of a String  returning
+ * an empty String if {@code null} input.</p>
+ *
+ * <p>This method removes whitespace.
+ * Whitespace is defined by {@link Character#isWhitespace(char)}.</p>
+ *
+ * <pre>
+ * StringUtils.stripToEmpty(null)     = ""
+ * StringUtils.stripToEmpty("")       = ""
+ * StringUtils.stripToEmpty("   ")    = ""
+ * StringUtils.stripToEmpty("abc")    = "abc"
+ * StringUtils.stripToEmpty("  abc")  = "abc"
+ * StringUtils.stripToEmpty("abc  ")  = "abc"
+ * StringUtils.stripToEmpty(" abc ")  = "abc"
+ * StringUtils.stripToEmpty(" ab c ") = "ab c"
+ * </pre>
+ *
+ * @param str  the String to be stripped, may be null
+ * @return the trimmed String, or an empty String if {@code null} input
+ */
+    public static String stripToEmpty(String str){
+        return StringUtils.stripToEmpty(str);
+    }
+
+}
