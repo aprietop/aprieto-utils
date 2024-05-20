faa3edb563f29c6cca34ae45a289131985230f63
Armando Prieto <aprieto@cryoport.com>
2024-05-20 15:12:11 -0700

    add greet

--- src/main/java/io/github/aprietop/StringUtils.java
+++ src/main/java/io/github/aprietop/StringUtils.java
@@ -2,29 +2,33 @@ package io.github.aprietop;
 
 public class StringUtils {
 
-/**
- * <p>Strips whitespace from the start and end of a String  returning
- * an empty String if {@code null} input.</p>
- *
- * <p>This method removes whitespace.
- * Whitespace is defined by {@link Character#isWhitespace(char)}.</p>
- *
- * <pre>
- * StringUtils.stripToEmpty(null)     = ""
- * StringUtils.stripToEmpty("")       = ""
- * StringUtils.stripToEmpty("   ")    = ""
- * StringUtils.stripToEmpty("abc")    = "abc"
- * StringUtils.stripToEmpty("  abc")  = "abc"
- * StringUtils.stripToEmpty("abc  ")  = "abc"
- * StringUtils.stripToEmpty(" abc ")  = "abc"
- * StringUtils.stripToEmpty(" ab c ") = "ab c"
- * </pre>
- *
- * @param str  the String to be stripped, may be null
- * @return the trimmed String, or an empty String if {@code null} input
- */
-    public static String stripToEmpty(String str){
+    /**
+     * <p>Strips whitespace from the start and end of a String  returning
+     * an empty String if {@code null} input.</p>
+     *
+     * <p>This method removes whitespace.
+     * Whitespace is defined by {@link Character#isWhitespace(char)}.</p>
+     *
+     * <pre>
+     * StringUtils.stripToEmpty(null)     = ""
+     * StringUtils.stripToEmpty("")       = ""
+     * StringUtils.stripToEmpty("   ")    = ""
+     * StringUtils.stripToEmpty("abc")    = "abc"
+     * StringUtils.stripToEmpty("  abc")  = "abc"
+     * StringUtils.stripToEmpty("abc  ")  = "abc"
+     * StringUtils.stripToEmpty(" abc ")  = "abc"
+     * StringUtils.stripToEmpty(" ab c ") = "ab c"
+     * </pre>
+     *
+     * @param str the String to be stripped, may be null
+     * @return the trimmed String, or an empty String if {@code null} input
+     */
+    public static String stripToEmpty(String str) {
         return org.apache.commons.lang3.StringUtils.stripToEmpty(str);
+    }
+
+    public static String greet(String str) {
+        return "Hello " + stripToEmpty(str);
     }
 
 }
